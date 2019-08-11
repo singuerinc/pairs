b23a79105093c6c98e59717961a2266ec35e8668
singuerinc <nahuel.scotti@gmail.com>
2019-08-11 13:32:56 +0200

    chore(deps): update deps

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('dist'));
+app.use(express.static("public"));
 
-app.get('/health', function (req, res) {
+app.get("/health", function(req, res) {
   res.sendStatus(200).end();
 });
 
-app.listen(process.env.NODE_PORT || 3000, process.env.NODE_IP || 'localhost', function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(
+  process.env.NODE_PORT || 3000,
+  process.env.NODE_IP || "localhost",
+  function() {
+    console.log("Example app listening on port 3000!");
+  }
+);
